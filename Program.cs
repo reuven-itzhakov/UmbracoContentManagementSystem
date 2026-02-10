5a13d2abd73bb6d589d1cd03cffd8d63a602a9ea
reuven.itzhakov <itzhakovreuven@gmail.com>
2026-02-10 10:33:01 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string contentRoot = builder.Environment.ContentRootPath;
+string dataDirectory = Path.Combine(contentRoot, "umbraco", "Data");
+string mediaDirectory = Path.Combine(contentRoot, "wwwroot", "media");
+string sqlitePath = Path.Combine(dataDirectory, "Umbraco.sqlite.db");
+
+Directory.CreateDirectory(dataDirectory);
+Directory.CreateDirectory(mediaDirectory);
+
+if (!File.Exists(sqlitePath))
+{
+    using FileStream _ = File.Create(sqlitePath);
+}
+
+AppDomain.CurrentDomain.SetData("DataDirectory", dataDirectory);
+builder.Configuration["ConnectionStrings:umbracoDbDSN"] =
+    $"Data Source={sqlitePath};Cache=Shared;Foreign Keys=True;Pooling=True";
+
+Console.WriteLine($"Umbraco DB path: {sqlitePath}");
+Console.WriteLine($"Umbraco DB DSN: {builder.Configuration["ConnectionStrings:umbracoDbDSN"]}");
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
